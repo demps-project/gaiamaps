1b72afa74ca0c9ef5e3ca977ae5f2176b9ed0301
gabriel-astudillo <gabriel.astudillo@gmail.com>
2024-11-13 23:09:35 -0300

    add index

---
+++ src/routes/(app)/gaiamaps/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>DEMPS | Main</title>
+	<meta name="description" content="DEMPS | Main" />
+	<meta http-equiv="refresh" content="0; URL=/gaiamaps/entorno" />
+</svelte:head>
+
+<section class="grid h-full place-items-center items-center justify-items-center">
+	<div class="-mt-40 text-center">
+		<h1 class="text-6xl font-bold leading-tight">DEMPS</h1>
+		<p class="text-lg text-slate-500">Disaster Evacuation and Mobility Patterns Simulator.</p>
+	</div>
+</section>
